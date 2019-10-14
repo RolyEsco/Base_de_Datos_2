f17a67403f60218ed4bed684e7702274b98e5342
RolyEsco <52805966+RolyEsco@users.noreply.github.com>
2019-10-14 13:23:13 -0400

    ULTIMO

--- creacion_tables.sql
+++ creacion_tables.sql
@@ -1,6 +1,9 @@
 
 https://github.com/RolyEsco/Base_de_Datos_2
 
+-- DDL Lenguaje de defifinion de datos
+--  create table
+-- alter table
 
 CREATE TABLE persona
 (id_persona serial PRIMARY KEY,
@@ -9,7 +12,7 @@ segundo_apellido varchar(30) NOT NULL,
 nombre varchar(40) NOT NULL,
 ci varchar(15) NOT NULL,
 fecha_nacimiento date not null,
-Activo boolean default true
+activo boolean default true
 );
 
 CREATE TABLE carrera
@@ -24,6 +27,20 @@ fecha_inscripcion date NOT NULL,
 id_carrera int REFERENCES carrera(id_carrera),
 id_persona int REFERENCES persona(id_persona)
 );
+
+-- DML  Lenguaje de Manipulacion de DAtos
+-- insert (insertar), update (actualizar), delete (eliminar)
+
+insert into persona values(primer_apellido,segundo_apellido,nombre,ci,fecha_nacimiento)
+values
+('ESCOBAR','COLQUE','ROLLY','4073157','1989-05-30'),
+('MAMANI','CHOQUE','MARIA','4032337','1999-12-12'),
+('ESCALERA','GUTIERREZ','MARCELA','5025457','1999-12-25');
+
+
+
+
+
 
 
 
