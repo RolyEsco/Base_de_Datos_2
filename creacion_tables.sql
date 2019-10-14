3c4dc434c581227ccd1059b21176c769c222ab48
RolyEsco <52805966+RolyEsco@users.noreply.github.com>
2019-10-14 12:27:00 -0400

    subiendo srcipt

---
+++ creacion_tables.sql
@@ -0,0 +1,24 @@
+
+
+CREATE TABLE persona
+(id_persona serial PRIMARY KEY,
+primer_apellido varchar(30) NOT NULL,
+segundo_apellido varchar(30) NOT NULL,
+nombre varchar(40) NOT NULL,
+ci varchar(15) NOT NULL,
+fecha_nacimiento date not null,
+Activo boolean default true
+);
+
+CREATE TABLE carrera
+(id_carrera SERIAL PRIMARY KEY,
+nombre VARCHAR(20) NOT NULL,
+mec boolean NOT NULL
+);
+
+CREATE TABLE estudiante
+(id_estudiante SERIAL PRIMARY KEY,
+fecha_inscripcion date NOT NULL,
+id_carrera int REFERENCES carrera(id_carrera),
+id_persona int REFERENCES persona(id_persona)
+);
