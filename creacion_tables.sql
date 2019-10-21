acf9e37f33cd4aef016f2d577ff3a76915735566
RolyEsco <52805966+RolyEsco@users.noreply.github.com>
2019-10-21 13:24:14 -0400

    ultima actualizacion

--- creacion_tables.sql
+++ creacion_tables.sql
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-=======
-
-https://github.com/RolyEsco/Base_de_Datos_2
-
--- DDL Lenguaje de defifinion de datos
---  create table
--- alter table
-
->>>>>>> fc94b380c7b141693fe735c8395dc5cdfb1ca89b
 CREATE TABLE persona
 (id_persona serial PRIMARY KEY,
 primer_apellido varchar(30) NOT NULL,
@@ -28,36 +18,40 @@ CREATE TABLE estudiante
 (id_estudiante SERIAL PRIMARY KEY,
 fecha_inscripcion date NOT NULL,
 id_carrera int REFERENCES carrera(id_carrera),
-id_persona int REFERENCES persona(id_persona)
+id_persona int REFERENCES persona(id_persona),
+activo boolean default true
 );
 
--- DML  Lenguaje de Manipulacion de DAtos
+-- DML  Lenguaje de Manipulacion de Datos
+-- ========================================
 -- insert (insertar), update (actualizar), delete (eliminar)
 
-<<<<<<< HEAD
--- insertando 6 registros a la tabla persona
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- Ingresando registros a la tabla persona
 
-=======
->>>>>>> fc94b380c7b141693fe735c8395dc5cdfb1ca89b
 insert into persona (primer_apellido,segundo_apellido,nombre,ci,fecha_nacimiento)
 values
 ('ESCOBAR','COLQUE','ROLLY','4073157','1989-05-30'),
 ('MAMANI','CHOQUE','MARIA','4032337','1999-12-12'),
-<<<<<<< HEAD
 ('ESCALERA','GUTIERREZ','MARCELA','5025457','1999-12-25'),
 ('YUCRA','HUERTA','MARISOL','4076187','1989-05-30'),
 ('JIMENEZ','ARCE','KATHIA MARIA','4882337','1990-11-06'),
 ('AJHUACHO','CHAMBI','SAMUEL ELIAS','4545555','1989-04-02');
 
 -- se nos olvido agregar el atributo genero
+-- alterando la tabla agregando una columna
 
 alter table persona add column genero varchar(9) not null;
 
--- actualizamos los datos de genero en los registros existentes
+-- Debemos actualizar los datos de genero en los registros existentes
 
 -- PRIMERA FORMA
-update persona set genero='FEMENINO'; -- CONSULTA menos eficiente ya que actualizara todos los registros
-update persona set genero='FEMENINO' where id_persona in (2,3,4,5); -- consulta eficiente
+
+update persona set genero='FEMENINO'; 
+-- CONSULTA NO eficiente ya que actualizara todos los registros
+
+update persona set genero='FEMENINO' where id_persona in (2,3,4,5); 
+-- consulta eficiente
 
 update persona set genero='MASCULINO' where id_persona in (1,6);
 
@@ -65,43 +59,57 @@ update persona set genero='MASCULINO' where id_persona in (1,6);
 -- drop table persona
 -- drop table materia
 
-select * from persona;
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- Ingresando carreras
 
-
-
-
-=======
-('ESCALERA','GUTIERREZ','MARCELA','5025457','1999-12-25');
->>>>>>> fc94b380c7b141693fe735c8395dc5cdfb1ca89b
-
-
-insert into carrera 
+insert into carrera (nombre,mec)
 values
-(5,'INGENIERIA DE REDES Y TELECOMUNICACION',TRUE),
-(15,'INGENIERIA DE SISTEMAS',FALSE),
+('INGENIERIA DE REDES Y TELECOMUNICACION',TRUE),
+('INGENIERIA DE SISTEMAS',FALSE),
+('DERECHO',TRUE),
+('ADMINISTRACION DE EMPRESAS',TRUE),
+('MARKETING',TRUE),
+('CONTADURIA PLUBLICA',TRUE),
+('INGENIERIA INDUSTRIAL',TRUE),
+('INGENIERIA MEDIO AMBIENTAL',FALSE);
+
+
+insert into carrera (nombre,mec)
+values
+('psicologia',FALSE);
+
+
+-- MUESTRA UN MENSAJE DE ERROR POR QUE LA SOBREPASAMOS LA LONGITUD EN NUMERO DE CARACTERES
+
+-- alterando la dimension de la columna nombre
+
+alter table carrera alter column nombre type varchar(40);
+
+
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- Ingresando regsitros a la tabla estudiante
+
+INSERT INTO estudiante
+(fecha_inscripcion,id_persona,id_carrera,activo)
+values
+('2019-01-02',1,1,true);
+('2019-01-02',2,2,true);
+('2019-01-02',3,1,true);
+('2019-01-02',4,4,true);
+('2019-01-02',5,6,true);
+('2019-01-02',6,2,true);
+
+-- sentencia para actualizar el campo autonumerico
 
 ALTER SEQUENCE campo_id_seq RESTART WITH 1;
 
 
 
-ALTER TABLE carrera ALTER COLUMN nombre TYPE varchar(40);
-
-insert into carrera 
-values
-(5,'INGENIERIA DE REDES Y TELECOMUNICACION',TRUE),
-(15,'INGENIERIA DE SISTEMAS',FALSE);
 
 
-insert into carrera (nombre,mec) 
-values
-('INGENIERIA INDUSTRIAL',TRUE);
 
 -- ingresando estudiantes
 
-INSERT INTO ESTUDIANTE
-(fecha_inscripcion,id_persona,id_carrera)
-values
-('2019-01-02',1,2);
 
 
 --  eliminando carrera de sistemas por contener errores en el id
@@ -125,19 +133,3 @@ WHERE id_carrera=3;
 update carrera set nombre=upper(nombre) 
 WHERE id_carrera in (3,6,70); -- between
 
-<<<<<<< HEAD
-=======
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> fc94b380c7b141693fe735c8395dc5cdfb1ca89b
