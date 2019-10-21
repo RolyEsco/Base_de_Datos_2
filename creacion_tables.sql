b6d3c5e474b7079a0adad33cbad39fa1895d3b50
RolyEsco <52805966+RolyEsco@users.noreply.github.com>
2019-10-21 12:54:02 -0400

    actualizacion de la base de datos

--- creacion_tables.sql
+++ creacion_tables.sql
@@ -1,5 +1,3 @@
-
-
 CREATE TABLE persona
 (id_persona serial PRIMARY KEY,
 primer_apellido varchar(30) NOT NULL,
@@ -7,7 +5,7 @@ segundo_apellido varchar(30) NOT NULL,
 nombre varchar(40) NOT NULL,
 ci varchar(15) NOT NULL,
 fecha_nacimiento date not null,
-Activo boolean default true
+activo boolean default true
 );
 
 CREATE TABLE carrera
@@ -22,3 +20,91 @@ fecha_inscripcion date NOT NULL,
 id_carrera int REFERENCES carrera(id_carrera),
 id_persona int REFERENCES persona(id_persona)
 );
+
+-- DML  Lenguaje de Manipulacion de DAtos
+-- insert (insertar), update (actualizar), delete (eliminar)
+
+-- insertando 6 registros a la tabla persona
+
+insert into persona (primer_apellido,segundo_apellido,nombre,ci,fecha_nacimiento)
+values
+('ESCOBAR','COLQUE','ROLLY','4073157','1989-05-30'),
+('MAMANI','CHOQUE','MARIA','4032337','1999-12-12'),
+('ESCALERA','GUTIERREZ','MARCELA','5025457','1999-12-25'),
+('YUCRA','HUERTA','MARISOL','4076187','1989-05-30'),
+('JIMENEZ','ARCE','KATHIA MARIA','4882337','1990-11-06'),
+('AJHUACHO','CHAMBI','SAMUEL ELIAS','4545555','1989-04-02');
+
+-- se nos olvido agregar el atributo genero
+
+alter table persona add column genero varchar(9) not null;
+
+-- actualizamos los datos de genero en los registros existentes
+
+-- PRIMERA FORMA
+update persona set genero='FEMENINO'; -- CONSULTA menos eficiente ya que actualizara todos los registros
+update persona set genero='FEMENINO' where id_persona in (2,3,4,5); -- consulta eficiente
+
+update persona set genero='MASCULINO' where id_persona in (1,6);
+
+-- drop table estudiante
+-- drop table persona
+-- drop table materia
+
+select * from persona;
+
+
+
+
+
+
+insert into carrera 
+values
+(5,'INGENIERIA DE REDES Y TELECOMUNICACION',TRUE),
+(15,'INGENIERIA DE SISTEMAS',FALSE),
+
+ALTER SEQUENCE campo_id_seq RESTART WITH 1;
+
+
+
+ALTER TABLE carrera ALTER COLUMN nombre TYPE varchar(40);
+
+insert into carrera 
+values
+(5,'INGENIERIA DE REDES Y TELECOMUNICACION',TRUE),
+(15,'INGENIERIA DE SISTEMAS',FALSE);
+
+
+insert into carrera (nombre,mec) 
+values
+('INGENIERIA INDUSTRIAL',TRUE);
+
+-- ingresando estudiantes
+
+INSERT INTO ESTUDIANTE
+(fecha_inscripcion,id_persona,id_carrera)
+values
+('2019-01-02',1,2);
+
+
+--  eliminando carrera de sistemas por contener errores en el id
+
+delete from carrera where id_carrera=15; 
+
+-- volviendo a insertar carrera de sistemas
+
+insert into carrera (nombre,mec) 
+values
+('INGENIERIA DE SISTEMAS',FALSE);
+
+
+-- actualizar regsitro de psicologia
+
+update carrera set nombre='PSICOLOGIA' 
+WHERE id_carrera=3;
+
+-- son equivalente
+
+update carrera set nombre=upper(nombre) 
+WHERE id_carrera in (3,6,70); -- between
+
